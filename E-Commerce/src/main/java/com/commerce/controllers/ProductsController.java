40a9e854cff85ee84a3abd64cf4ca1a2e8875d0c
Igor Dzyombak <dzyombak.igor@gmail.com>
2016-07-13 13:21:59 +0300

    Iteration 22 - done

--- E-Commerce/src/main/java/com/commerce/controllers/ProductsController.java
+++ E-Commerce/src/main/java/com/commerce/controllers/ProductsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class ProductsController {
     private ProductsService productsService;
 
     @RequestMapping(value = "/loadListOfAllProducts", method = RequestMethod.GET)
+    @ResponseBody
     public List<ProductsModel> loadListOfAllProducts() {
         return productsService.getListOfProducts();
     }
