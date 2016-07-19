45078b4a6ad2b3d751fa6b729211d3fbcf9b2634
Igor Dzyombak <dzyombak.igor@gmail.com>
2016-07-19 15:30:45 +0300

    Iterations 33 - done

--- E-Commerce/src/main/java/com/commerce/controllers/CategoriesController.java
+++ E-Commerce/src/main/java/com/commerce/controllers/CategoriesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -16,8 +17,13 @@ public class CategoriesController {
     private CategoriesService categoriesService;
 
     @RequestMapping(value = "/loadListOfAllCategories", method = RequestMethod.GET)
+    @ResponseBody
     public List<CategoriesModel> loadListOfAllCategories() {
         return categoriesService.getListOfCategories();
     }
 
+    @RequestMapping(value = "/allcategories")
+    public String getAllCategoriesPage() {
+        return "allCategories";
+    }
 }
---
+++ E-Commerce/src/main/webapp/WEB-INF/pages/allCategories.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>E-Commerce. All categories table</title>
+    <script src="resources/vendor/js/jquery-2.2.2.js" type="application/javascript"></script>
+    <script src="resources/custom/js/allCategoriesJS.js" type="application/javascript"></script>
+</head>
+<body>
+Welcome to the categories page.
+<br/>
+<br/>
+<br/>
+<table id="Categories table" class="table table-bordered" border="1">
+    <thead>
+    <tr>
+        <th>&nbsp; Cities &nbsp;</th>
+    </tr>
+    </thead>
+    <tbody id="allCategoriesTable" align="center">
+    </tbody>
+</table>
+</body>
+</html>
